257e235ea70dcbe74f806947c9b163f0af4a070d
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-24 16:06:17 -0800

    grab the type event out

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/jeffail/gabs"
 )
 
 func main() {
@@ -27,9 +29,19 @@ func main() {
 
 	scanner := bufio.NewScanner(gz)
 	i := 0
+	var value string
+	var ok bool
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		jsonParsed, err := gabs.ParseJSON([]byte(scanner.Text()))
+		if err != nil {
+			fmt.Println("ERROR")
+		}
+
+		value, ok = jsonParsed.Path("type").Data().(string)
+		fmt.Println(jsonParsed)
 		fmt.Println("")
+		fmt.Println(value)
+		fmt.Println(ok)
 		i++
 		if i > 2 {
 			break
